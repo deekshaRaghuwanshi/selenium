12ed6452f96a5e818da252a150b835163abecbb7
Vikram Bairi <vikram.b@techmojo.in>
2020-04-11 21:25:40 +0530

    -ve+veNumber

---
+++ practiceprograms/src/practiceprograms/PositiveNegativeNumber.java
@@ -0,0 +1,15 @@
+package practiceprograms;
+
+public class PositiveNegativeNumber {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		if (a>0) 
+		{
+			System.out.println("Number is Positive");
+		}
+
+	}
+
+}
