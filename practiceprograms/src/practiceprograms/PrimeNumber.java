2feba2f279fb793844dbe36757ca558139ceeb9b
Vikram Bairi <vikram.b@techmojo.in>
2020-04-11 21:33:55 +0530

    evenaddnumber

---
+++ practiceprograms/src/practiceprograms/PrimeNumber.java
@@ -0,0 +1,19 @@
+package practiceprograms;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		
+		int a =9;
+		
+		if (a %2==0)
+		{
+			System.out.println("Given number is even");
+		}
+		else 
+		{
+			System.out.println("Given number is odd");
+		}
+	}
+
+}
