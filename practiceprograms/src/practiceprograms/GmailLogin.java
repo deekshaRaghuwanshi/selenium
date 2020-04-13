b4af372a4f29192c3369d2708f6084781ffb6884
Vikram Bairi <vikram.b@techmojo.in>
2020-04-13 17:56:58 +0530

    gmail login & logout

--- practiceprograms/src/practiceprograms/GmailLogin.java
+++ practiceprograms/src/practiceprograms/GmailLogin.java
@@ -1,5 +1,7 @@
 package practiceprograms;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
@@ -8,21 +10,54 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class GmailLogin {
 
 	public static void main(String[] args) throws InterruptedException {
-		
+
 		System.setProperty("webdriver.chrome.driver","/Users/vikram.b/UI_Automation/chromedriver");
 		ChromeDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.gmail.com/");
 		Thread.sleep(5000);
+		
 		//Do login
 		driver.findElement(By.id("identifierId")).sendKeys("selenium0004",Keys.ENTER);
 		Thread.sleep(3000);
 		driver.findElement(By.xpath("//*[@name='password']")).sendKeys("Selenium@321",Keys.ENTER);
 		Thread.sleep(3000);
-		WebElement email = driver.findElement(By.xpath("(//table)[7]/tbody/tr/td[5]/div[2]/span/span"));
-		String yahooemail=email.getAttribute("email");
-		System.out.println(yahooemail);
+		
+		// collect list of mails 
+		List<WebElement> allmails = driver.findElements(By.xpath("(//table)[7]/tbody/tr"));
+		System.out.println("Total mails account in inbox "+allmails.size()+"\n------------");
+		
+		// read from email from each and every mail using Attribute
+		for (WebElement e:allmails)
+		{
+			
+			String actualemail = e.findElement(By.xpath("td[5]/div[2]/span/span")).getAttribute("email");
+			//print from emailid
+			
+			// check read email and given email are equals
+			String expectemail ="googlecommunityteam-noreply@google.com";
+			if(actualemail.equals(expectemail))
+			{
+				System.out.println("Actual email is "+actualemail);
+				System.out.println("Expected mail is "+expectemail);
+				System.out.println("Both are same. Hence Test Passed");
+				break;
+			}
+			else {
+				System.out.println("Actual email is "+actualemail);
+				System.out.println("Expected email is "+expectemail);
+				System.out.println("Both are different. Hence Test failed \n---------------------");
+			}
+		}
+		
+		// do logout from gmail
+		
+		driver.findElement(By.xpath("//a[contains(@aria-label,'Google Account')]")).click();
+		driver.findElement(By.xpath("//a[text()='Sign out']")).click();
+		Thread.sleep(5000);
 		driver.close();
-	}
 
+	}
 }
+
+
--- practiceprograms/src/practiceprograms/YahooToGmail.java
+++ practiceprograms/src/practiceprograms/YahooToGmail.java
@@ -1,7 +1,12 @@
 package practiceprograms;
 
+import java.util.ArrayList;
+import java.util.Set;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.netty.util.internal.ThreadExecutorMap;
@@ -19,10 +24,11 @@ public class YahooToGmail {
 		Thread.sleep(5000);
 		driver.findElement(By.xpath("//a/span[text()='Sign in']")).click();
 		Thread.sleep(6000);
+		// do login into yahoo
 		driver.findElement(By.id("login-username")).sendKeys("bairivikram",Keys.ENTER);	
 		Thread.sleep(6000);
 		driver.findElement(By.id("login-passwd")).sendKeys("Goud@1992",Keys.ENTER);
-	
+		// compose email to gmail
 		Thread.sleep(5000);
 		driver.findElement(By.xpath("//a[text()='Compose']")).click();
 		Thread.sleep(2000);
@@ -31,7 +37,23 @@ public class YahooToGmail {
 		driver.findElement(By.xpath("//div[@aria-label='Message body']/div")).sendKeys("Hi SDET");
 		driver.findElement(By.xpath("//button/span[text()='Send']")).click();
 		System.out.println("Successful sent mail");
+		//switch to new tab in current browser
+		driver.switchTo().newWindow(WindowType.TAB);
+		Set<String> s = driver.getWindowHandles();
+		ArrayList<String> l =new ArrayList<String>(s);
+		driver.switchTo().window(l.get(1));
+		driver.get("https://www.gmail.com/");
+		Thread.sleep(5000);
+		//Do login
+		driver.findElement(By.id("identifierId")).sendKeys("selenium0004",Keys.ENTER);
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//*[@name='password']")).sendKeys("Selenium@321",Keys.ENTER);
+		Thread.sleep(3000);
+		WebElement email = driver.findElement(By.xpath("(//table)[7]/tbody/tr/td[5]/div[2]/span/span"));
+		String yahooemail=email.getAttribute("email");
 		
+		
+		System.out.println(yahooemail);
 		
 		driver.close();
 		
